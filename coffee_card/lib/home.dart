d575f475aa9ce66f914d935392dd8999cf66f943
OhangaDon <ochieng.vincent2022@students.jkuat.ac.ke>
2024-10-12 01:12:37 -0400

    commit

--- coffee_card/lib/home.dart
+++ coffee_card/lib/home.dart
@@ -1,4 +1,6 @@
+import 'package:coffee_card/styled_body_text.dart';
 import 'package:flutter/material.dart';
+import 'package:coffee_card/coffee_prefs.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,27 +9,32 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text(
-            'My Coffee Id',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
+          title: const Text('My Coffee Id',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
           backgroundColor: Colors.brown[700],
           centerTitle: true,
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              color: Colors.brown[200],
-              padding: const EdgeInsets.all(20),
-              child: const Text('How i like my coffee...'),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Container(
+            color: Colors.brown[200],
+            padding: const EdgeInsets.all(20),
+            child: const StyledBodyText('How i like my coffee'),
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: const EdgeInsets.all(20),
+            child: const CoffeePrefs(),
+          ),
+          Expanded(
+            child: Image.asset(
+              'assets/img/coffee_bg.jpg',
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.bottomCenter,
             ),
-            Container(
-              color: Colors.brown[100],
-              padding: const EdgeInsets.all(20),
-              child: const Text('Coffee prefs.'),
-            )
-          ],
-        ));
+          ),
+        ]));
   }
 }
--- coffee_card/lib/main.dart
+++ coffee_card/lib/main.dart
@@ -2,9 +2,12 @@ import 'package:coffee_card/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Home()));
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
 }
 
+// sandbox
 class Sandbox extends StatelessWidget {
   const Sandbox({super.key});
 
@@ -15,27 +18,26 @@ class Sandbox extends StatelessWidget {
         title: const Text('Sandbox'),
         backgroundColor: Colors.grey,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 100,
-            color: Colors.red,
-            child: const Text("one"),
-          ),
-          Container(
-            width: 200,
-            color: Colors.green,
-            child: const Text("two"),
-          ),
-          Container(
-            width: 300,
-            color: Colors.blue,
-            child: const Text("three"),
-          )
-        ],
-      ),
+      body: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: 100,
+              color: Colors.red,
+              child: const Text('one'),
+            ),
+            Container(
+              height: 200,
+              color: Colors.green,
+              child: const Text('two'),
+            ),
+            Container(
+              height: 300,
+              color: Colors.blue,
+              child: const Text('three'),
+            )
+          ]),
     );
   }
 }
